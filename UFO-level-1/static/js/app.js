78fdaa7bdb4699b3367abe1ce53529e0111c9730
Nehemias Vicente <nvnehemias@gmail.com>
2019-09-06 16:02:31 -0700

    Adding information to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,3 +8,12 @@ console.log(data)
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
+tableData.forEach((data_info) => {
+    console.log(data_info);
+    var row = tbody.append("tr");
+    Object.entries(data_info).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    })
+});
